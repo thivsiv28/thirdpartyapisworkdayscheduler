960e2fc30f8b271813c71f5649fd401afe1ff9ce
TSIV28 <thiviya.sivasithamparam@gmail.com>
2022-09-23 19:02:18 -0400

    testing git addbug wouldnt let me add

---
+++ assets/javascript/script.js
@@ -0,0 +1,41 @@
+
+
+
+var currentDate = moment().format("dddd, MMMM Do, YYYY");
+$("#currentDay").text(currentDate);
+
+
+//check time
+
+var currentHour = moment().format("H");
+//if time is right now 
+
+for (var i = 9; i < 17; i++) {
+
+}
+
+
+//make box green
+//if time is past
+
+// if (time right now is == currenttime ) {
+
+//     then make the colors green for present;
+// }
+
+// else if ( time right now is before right now) {
+
+//     then make the colors red for parse;
+// }
+
+// else if (time right now is after time then make it grey) {
+
+// }
+
+//make row red
+//if time is future 
+//make box grey
+//save items into local storage 
+//reload page and items should be there
+//clicking button saves task
+
